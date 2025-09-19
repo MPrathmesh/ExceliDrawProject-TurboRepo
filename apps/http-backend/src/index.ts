a26850aac6b8c57b8805f71ff0d84cc8cbb11873
Prathamesh More <moreprathamesh564@gmail.com>
2025-09-19 20:24:49 +0530

    create packages and used in backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middleware";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
+
 const app = express();
 
 app.post("/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message : "Incorrect Inputs"
+        })
+        return;
+    }
+    
     //db call
     res.json({
         userId : "123"
@@ -12,6 +22,14 @@ app.post("/signup", (req, res) => {
 })
 
 app.post("/signin", (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message : "Incorrect Inputs"
+        })
+        return;
+    }
+
     const userId = 1;
     const token = jwt.sign({
         userId
@@ -23,6 +41,14 @@ app.post("/signin", (req, res) => {
 })
 
 app.post("/room", middleware, (req, res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message : "Incorrect Inputs"
+        })
+        return;
+    }
+
     //db call
     res.json({
         roomId : "123"
